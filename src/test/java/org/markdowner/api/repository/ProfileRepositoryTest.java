16a16658113269a3404a530cf5e8bc3e1d18c0d5
evertonbrunosds <evertonbrunosds@gmail.com>
2025-08-11 22:33:21 -0300

    testa os métodos de busca com e sem paginação

--- src/test/java/org/markdowner/api/repository/ProfileRepositoryTest.java
+++ src/test/java/org/markdowner/api/repository/ProfileRepositoryTest.java
@@ -3,6 +3,7 @@ package org.markdowner.api.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,21 +21,118 @@ public class ProfileRepositoryTest {
 
     @Test
     @DisplayName("não deve encontrar um perfil através do email")
-    public void shouldNotFindProfileByEmailTest() {
+    public void found_findByEmailTest() {
         assertThat(repository.findByEmail("not_found@example.org")).isNotPresent();
     }
 
     @Test
-    @DisplayName("deve encontrar um perfil através do email")
-    public void shouldFindProfileByEmailTest() {
-        final var builder = Profile.builder()
-                .id(UUID.fromString("0198a0d3-bd61-7000-9b88-50cc3638e965"))
-                .name("Bruno Borges")
-                .description("Apaixonado por tecnologia e música.")
-                .birthday(LocalDate.of(1992, 04, 23))
-                .email("bruno@example.com")
-                .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW");
-        assertThat(repository.findByEmail("bruno@example.com")).isEqualTo(Optional.of(builder.build()));
+    @DisplayName("deve encontrar todos os perfis do settlement.sql pelo email")
+    public void notFound_findByEmailTest() {
+        assertThat(repository.findByEmail("alice@example.com")).isEqualTo(Optional.of(Profile.builder()
+                .id(UUID.fromString("01989bad-6161-7000-0ae9-f440b10578ec"))
+                .name("Alice Alves")
+                .description("Uma pessoa criativa e dedicada.")
+                .birthday(LocalDate.of(1988, 1, 10))
+                .email("alice@example.com")
+                .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build()));
+    }
+
+    @Test
+    @DisplayName("deve buscar nomes por ordem alfabética")
+    public void foundLimit10_findByNameContainingIgnoreCaseTest() {
+        assertThat(repository.findByNameContainingIgnoreCase(6, "ana")).isEqualTo(List.of(
+                Profile.builder()
+                        .id(UUID.fromString("019b248e-a961-7000-233d-b99937ef11d4"))
+                        .name("Ana Isidoro")
+                        .description("Especialista em segurança de aplicações.")
+                        .birthday(LocalDate.of(1983, 12, 25))
+                        .email("ana.isidoro@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("019926b9-1561-7000-6090-c113559eb471"))
+                        .name("Diana Fernandes")
+                        .description("Especialista em e-commerce e vendas.")
+                        .birthday(LocalDate.of(1995, 4, 13))
+                        .email("diana@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("019acd02-8d61-7000-dc08-f222fa8c30f4"))
+                        .name("Juliana Rios")
+                        .description("Estudante de engenharia de dados.")
+                        .birthday(LocalDate.of(2000, 2, 9))
+                        .email("juliana.rios@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("0199d5d1-4d61-7000-18f0-9689e06c1745"))
+                        .name("Mariana Paz")
+                        .description("Estudante de engenharia de dados.")
+                        .birthday(LocalDate.of(2000, 12, 1))
+                        .email("mariana.paz@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("019a7050-1561-7000-762c-4de2f6a38808"))
+                        .name("Renata Yamanaka")
+                        .description("Analista de dados e ciência de dados.")
+                        .birthday(LocalDate.of(1994, 5, 16))
+                        .email("renata.yamanaka@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("01997e45-3161-7000-4774-f18b86a14137"))
+                        .name("Vitor Viana")
+                        .description("Consultor financeiro e investidor.")
+                        .birthday(LocalDate.of(1982, 10, 11))
+                        .email("vitor.viana@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build()));
+    }
+
+    @Test
+    @DisplayName("deve buscar nomes por ordem alfabética")
+    public void foundLimit02_findByNameContainingIgnoreCaseTest() {
+        assertThat(repository.findByNameContainingIgnoreCase(2, "ana")).isEqualTo(List.of(
+                Profile.builder()
+                        .id(UUID.fromString("019b248e-a961-7000-233d-b99937ef11d4"))
+                        .name("Ana Isidoro")
+                        .description("Especialista em segurança de aplicações.")
+                        .birthday(LocalDate.of(1983, 12, 25))
+                        .email("ana.isidoro@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("019926b9-1561-7000-6090-c113559eb471"))
+                        .name("Diana Fernandes")
+                        .description("Especialista em e-commerce e vendas.")
+                        .birthday(LocalDate.of(1995, 4, 13))
+                        .email("diana@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build()));
+        assertThat(repository.findByNameContainingIgnoreCase(2, "Diana Fernandes", "ana")).isEqualTo(List.of(
+                Profile.builder()
+                        .id(UUID.fromString("019acd02-8d61-7000-dc08-f222fa8c30f4"))
+                        .name("Juliana Rios")
+                        .description("Estudante de engenharia de dados.")
+                        .birthday(LocalDate.of(2000, 2, 9))
+                        .email("juliana.rios@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("0199d5d1-4d61-7000-18f0-9689e06c1745"))
+                        .name("Mariana Paz")
+                        .description("Estudante de engenharia de dados.")
+                        .birthday(LocalDate.of(2000, 12, 1))
+                        .email("mariana.paz@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build()));
+        assertThat(repository.findByNameContainingIgnoreCase(2, "Mariana Paz", "ana")).isEqualTo(List.of(
+                Profile.builder()
+                        .id(UUID.fromString("019a7050-1561-7000-762c-4de2f6a38808"))
+                        .name("Renata Yamanaka")
+                        .description("Analista de dados e ciência de dados.")
+                        .birthday(LocalDate.of(1994, 5, 16))
+                        .email("renata.yamanaka@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build(),
+                Profile.builder()
+                        .id(UUID.fromString("01997e45-3161-7000-4774-f18b86a14137"))
+                        .name("Vitor Viana")
+                        .description("Consultor financeiro e investidor.")
+                        .birthday(LocalDate.of(1982, 10, 11))
+                        .email("vitor.viana@example.com")
+                        .password("$2a$10$7Z0zPEtZklljGNH8JHcnRO0pOZAVlBH36Fg7QO9N1LD4thimBL.TW").build()));
     }
 
 }
