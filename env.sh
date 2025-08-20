00c582eb62e4cee61e89f34c7f6f3421062940fd
evertonbrunosds <evertonbrunosds@gmail.com>
2025-08-20 00:03:35 -0300

    criação de variáveis de ambiente para o ambiente linux

---
+++ env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROFILE="$HOME/.profile"
+
+# Lista de variáveis no formato VAR=valor
+declare -A vars=(
+  [POSTGRES_IPV4]="127.0.0.1"
+  [POSTGRES_PORT]="5432"
+  [POSTGRES_DB]="markdowner-db"
+  [POSTGRES_USER]="postgres"
+  [POSTGRES_PASSWORD]="123456"
+  [DATASOURCE_USERNAME]='${POSTGRES_USER}'
+  [DATASOURCE_PASSWORD]='${POSTGRES_PASSWORD}'
+  [DATASOURCE_URL]='jdbc:postgresql://${POSTGRES_IPV4}:${POSTGRES_PORT}/${POSTGRES_DB}'
+  [DDL_AUTO]="create"
+)
+
+for key in "${!vars[@]}"; do
+  value="${vars[$key]}"
+
+  # Se já existir e for diferente → substitui
+  if grep -qE "^export $key=" "$PROFILE"; then
+    current_val=$(grep -E "^export $key=" "$PROFILE" | sed -E "s/^export $key=//")
+    if [[ "$current_val" != "$value" ]]; then
+      sed -i -E "s|^export $key=.*|export $key=$value|" "$PROFILE"
+      echo "Updated: $key"
+    fi
+  else
+    # Se não existir, adiciona
+    echo "export $key=$value" >> "$PROFILE"
+    echo "Created: $key"
+  fi
+done
