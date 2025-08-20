fad27ee64e9344ffbb5090587757a6eb12ab824d
evertonbrunosds <evertonbrunosds@gmail.com>
2025-08-20 00:16:07 -0300

    .

--- src/test/java/org/markdowner/api/ApiApplicationTests.java
+++ src/test/java/org/markdowner/api/ApiApplicationTests.java
@@ -1,7 +1,5 @@
 package org.markdowner.api;
 
-import java.util.Locale;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,6 @@ class ApiApplicationTests {
 
     @Test
     void contextLoads() {
-        Locale.setDefault(Locale.ENGLISH);
     }
 
 }
