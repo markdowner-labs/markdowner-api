72b0a5322078eeec521b32709c0a9501d0523d78
evertonbrunosds <evertonbrunosds@gmail.com>
2025-08-21 21:54:09 -0300

    testes a serem implementados

---
+++ src/test/java/org/markdowner/api/domain/validation/profile/BirthdayTest.java
@@ -0,0 +1,16 @@
+package org.markdowner.api.domain.validation.profile;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BirthdayTest {
+
+    @Test
+    public void fail() {
+        assertTrue(false, "teste não implementado");
+    }
+
+}
---
+++ src/test/java/org/markdowner/api/domain/validation/profile/DescriptionTest.java
@@ -0,0 +1,16 @@
+package org.markdowner.api.domain.validation.profile;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DescriptionTest {
+
+    @Test
+    public void fail() {
+        assertTrue(false, "teste não implementado");
+    }
+
+}
