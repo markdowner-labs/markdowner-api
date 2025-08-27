82a290cfb01fef5d71725c29f091b3f6f6158eec
evertonbrunosds <evertonbrunosds@gmail.com>
2025-08-26 23:11:23 -0300

    get para profile

---
+++ src/main/java/org/markdowner/api/controller/ProfileController.java
@@ -0,0 +1,56 @@
+package org.markdowner.api.controller;
+
+import static java.util.Objects.nonNull;
+import static org.springframework.util.StringUtils.hasText;
+
+import java.util.UUID;
+
+import org.markdowner.api.service.ProfileService;
+import org.markdowner.api.util.Routes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(Routes.PROFILE)
+public class ProfileController {
+    private final ProfileService service;
+
+    @GetMapping
+    public ResponseEntity<?> get(
+            @RequestParam(required = false) final UUID id,
+            @RequestParam(required = false) final String email,
+            @RequestParam(required = false) final String name,
+            @RequestParam(required = false) final String lastSeenName,
+            @RequestParam(required = false) final UUID lastSeenId,
+            @RequestParam(defaultValue = "100", required = false) final int limit) {
+        if (nonNull(id)) {
+            return ResponseEntity.of(service.findById(id));
+        }
+        if (hasText(email)) {
+            return ResponseEntity.of(service.findByEmail(email));
+        }
+        if (hasText(name)) {
+            if (hasText(lastSeenName) || nonNull(lastSeenId)) {
+                return ResponseEntity.ok(service.findByNameContainingIgnoreCase(limit, lastSeenName, lastSeenId, name));
+            }
+            return ResponseEntity.ok(service.findByNameContainingIgnoreCase(limit, name));
+        }
+        if (hasText(lastSeenName) || nonNull(lastSeenId)) {
+            return ResponseEntity.ok(service.findAll(limit, lastSeenName, lastSeenId));
+        }
+        return ResponseEntity.ok(service.findAll(limit));
+    }
+
+}
+
+/*
+ * 1. Criar fixture de build de profiles.
+ * 2. Testar individualmente cada parâmetro do método get.
+ * 3. Criar capturador de exceções.
+ */
---
+++ src/main/java/org/markdowner/api/util/Routes.java
@@ -0,0 +1,10 @@
+package org.markdowner.api.util;
+
+
+public class Routes {
+
+    private static final String PATH = "/api/v0";
+
+    public static final String PROFILE = PATH + "/profile";
+
+}
