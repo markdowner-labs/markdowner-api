e229644be8d581cecc195ae11d73286802ba9862
evertonbrunosds <evertonbrunosds@gmail.com>
2025-09-10 18:35:27 -0300

    .

--- src/test/java/org/markdowner/api/controller/ProfileControllerTest.java
+++ src/test/java/org/markdowner/api/controller/ProfileControllerTest.java
@@ -29,11 +29,11 @@ public class ProfileControllerTest {
 
         @ParameterizedTest
         @CsvSource(value = {
-                "Id: identificador inválido;0199125e-z345-719b-8607-7eb3769dbfa6",
-                "Id: identificador mal formado;0199125e-z345-719b-8607"
+                        "Id: identificador inválido;0199125e-z345-719b-8607-7eb3769dbfa6",
+                        "Id: identificador mal formado;0199125e-z345-719b-8607"
         }, delimiter = ';')
-        @DisplayName("Validação do campo Id - casos inválidos")
-        void shouldFindProfilesByIdTest(final String description, final String id) throws Exception {
+        @DisplayName("Deve retornar erro 400 quando o ID é inválido")
+        void shouldReturnBadRequestWhenIdIsInvalid(final String description, final String id) throws Exception {
                 final var request = MockMvcRequestBuilders
                                 .get(Routes.PROFILE)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -70,8 +70,8 @@ public class ProfileControllerTest {
                         "Email: domínio iniciando com ponto;juliana.rios@.example.com",
                         "Email: endereço de email com mais de 76 caracteres;rprsgefxrayrpaydsncgdxpsnhwpryazfrrbjzkugvqimfidwpcjiyapipyvrehko@example.com",
         }, delimiter = ';')
-        @DisplayName("Validação do campo Email - casos inválidos")
-        void shouldFindProfilesByEmailTest(final String description, final String email) throws Exception {
+        @DisplayName("Deve retornar erro 400 quando o e-mail é inválido")
+        void shouldReturnBadRequestWhenEmailIsInvalid(final String description, final String email) throws Exception {
                 final var request = MockMvcRequestBuilders
                                 .get(Routes.PROFILE)
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -83,6 +83,26 @@ public class ProfileControllerTest {
                                 .andReturn().getResponse().getContentAsString();
 
                 assertEquals("{\"email\":[\"deve ser um endereço de e-mail bem formado\"]}", response, description);
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                        "Id: identificador inválido;0199125e-z345-719b-8607-7eb3769dbfa6",
+                        "Id: identificador mal formado;0199125e-z345-719b-8607"
+        }, delimiter = ';')
+        @DisplayName("Deve retornar erro 400 quando o lastSeenId é inválido")
+        void shouldReturnBadRequestWhenLastSeenIdIsInvalid(final String description, final String lastSeenId) throws Exception {
+                final var request = MockMvcRequestBuilders
+                                .get(Routes.PROFILE)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .queryParam("lastSeenId", lastSeenId);
+
+                final var response = mockMvc.perform(request)
+                                .andExpect(status().isBadRequest())
+                                .andReturn().getResponse().getContentAsString();
+
+                assertEquals("{\"lastSeenId\":\"deve pertencer ao tipo UUID\"}", response, description);
         }
 
 }
